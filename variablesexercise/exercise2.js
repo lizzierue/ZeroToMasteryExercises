7f83ca3a0d2580bde0fe3211242a6e0b074ed394
Elizabeth Samatepo <elizabeth.samatepo@tafensw.edu.au>
2020-07-05 12:53:10 +1000

    VARIABLES EXERCISE ZERO TO MASTERY

---
+++ variablesexercise/exercise2.js
@@ -0,0 +1,13 @@
+//add variable "firstName" and "lastName"  so that they equal your name
+
+let firstName = "Elizabeth"
+let lastName = "Samatepo";
+
+console.log (firstName + " "  + lastName);
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+// a + b // what is the answer here?
+console .log (a + b);
